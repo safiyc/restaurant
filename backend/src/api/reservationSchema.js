be3621a738cb1b60e8c4116e297cbf7ffcfe3284
Luciano Oliveira <loliveira@irobot.com>
2019-08-24 23:31:21 -0400

    fix reserve form

--- backend/src/api/reservationSchema.js
+++ backend/src/api/reservationSchema.js
@@ -3,10 +3,10 @@ const mongoose = restful.mongoose;
 
 const reservationSchema = new mongoose.Schema({
     partySize: { type: Number, require: true},
-    name: { type: String, require: true},
-    date: { type: Date, required: true},
-    hour: { type: Number, min: 10, max: 23, required: true },
-    minutes: { type: Number, min: 00, max: 45, required: true }
+    partyName: { type: String, require: true},
+    // date: { type: Date, required: true},
+    // hour: { type: Number, min: 10, max: 23, required: false },
+    // minutes: { type: Number, min: 00, max: 45, required: false }
 });
 
 module.exports = restful.model('reservationSchema', reservationSchema);
